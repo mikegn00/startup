94d8694654a1bad26404c83f34cd74a56cee486b
Michael Morris <michaelgn00@gmail.com>
2020-12-17 08:47:23 +0000

    initial with info and getting apis

---
+++ src/YugiohCardDetails.java
@@ -0,0 +1,9 @@
+public class YugiohCardDetails {
+    String name, desc, rarity;
+    YugiohCardDetails(String name, String desc, String rarity){
+        this.name = name;
+        this.desc = desc;
+        this.rarity = rarity;
+    }
+
+}
